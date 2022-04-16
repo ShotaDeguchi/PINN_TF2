1e3a6914aac0363b17d4c43e2a80c994891d3d76
shota <shotadeguchio2o5@gmail.com>
2022-04-16 18:06:20 +0900

    measure time

--- 04_diffusion/params.py
+++ 04_diffusion/params.py
@@ -26,9 +26,9 @@ N_b = int(1e3)   # evaluates boundary condition
 N_r = int(1e4)   # evaluates PDE residual within the domain
 
 
-N_0 = int(1e4)   # evaluates initial condition
-N_b = int(1e4)   # evaluates boundary condition
-N_r = int(5e4)   # evaluates PDE residual within the domain
+N_0 = int(2e4)   # evaluates initial condition
+N_b = int(3e4)   # evaluates boundary condition
+N_r = int(1e5)   # evaluates PDE residual within the domain
 
 
 # optimization
